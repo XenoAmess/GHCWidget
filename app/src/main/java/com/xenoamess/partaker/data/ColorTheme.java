4dbc1f2d45efd8f510c66506c3f49f3a6bcb2b3b
XenoAmess <912890489@qq.com>
2018-03-14 04:54:53 +0800

    fix

--- app/src/main/java/com/xenoamess/partaker/data/ColorTheme.java
+++ app/src/main/java/com/xenoamess/partaker/data/ColorTheme.java
@@ -15,9 +15,11 @@ public class ColorTheme {
     public static String RED = "Red";
     public static String BLUE = "Blue";
     public static String PURPLE = "Purple";
+    public static String YELLOW = "Yellow";
     public static String ORANGE = "Orange";
     public static String HALLOWEEN = "Halloween";
-
+    public static String PSYCHEDELIC = "Psychedelic";
+    public static String MOON = "Moon";
 
     private String[] standard = {"#ebedf0", "#c6e48b", "#7bc96f", "#239a3b", "#196127"};
     private String[] modern = {"#afaca8", "#d6e685", "#8cc665", "#44a340", "#1e6823"};
@@ -25,21 +27,27 @@ public class ColorTheme {
     private String[] red = {"#eeeeee", "#ff7171", "#ff0000", "#b70000", "#830000"};
     private String[] blue = {"#eeeeee", "#6bcdff", "#00a1f3", "#0079b7", "#003958"};
     private String[] purple = {"#eeeeee", "#d2ace6", "#aa66cc", "#660099", "#4f2266"};
+    private String[] yellow = {"#eeeeee", "#d7d7a2", "#d4d462", "#e0e03f", "#ffff00"};
     private String[] orange = {"#eeeeee", "#ffcc80", "#ffa726", "#fb8c00", "#e65100"};
     private String[] halloween = {"#eeeeee", "#ffee4a", "#ffc501", "#fe9600", "#03001c"};
+    private String[] psychedelic = {"#eeeeee", "#faafe1", "#fb6dcc", "#fa3fbc", "#ff00ab"};
+    private String[] moon = {"#eeeeee", "#6bcdff", "#00a1f3", "#48009a", "#4f2266"};
 
     private HashMap<String, String[]> themeMap;
 
     public ColorTheme() {
-        themeMap = new HashMap<String, String[]>();
-        themeMap.put(ColorTheme.GITHUB, standard);
-        themeMap.put(ColorTheme.MODERN, modern);
-        themeMap.put(ColorTheme.GRAY, gray);
-        themeMap.put(ColorTheme.RED, red);
-        themeMap.put(ColorTheme.BLUE, blue);
-        themeMap.put(ColorTheme.PURPLE, purple);
-        themeMap.put(ColorTheme.ORANGE, orange);
-        themeMap.put(ColorTheme.HALLOWEEN, halloween);
+        themeMap = new HashMap<>();
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.GITHUB, standard);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.MODERN, modern);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.GRAY, gray);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.RED, red);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.BLUE, blue);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.PURPLE, purple);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.YELLOW, yellow);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.ORANGE, orange);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.HALLOWEEN, halloween);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.PSYCHEDELIC, psychedelic);
+        themeMap.put(com.xenoamess.partaker.data.ColorTheme.MOON, moon);
     }
 
     public int getColor(String themeName, int level) {
@@ -47,7 +55,7 @@ public class ColorTheme {
     }
 
     public static CharSequence[] getThemeNames() {
-        return new CharSequence[]{ColorTheme.GITHUB, ColorTheme.MODERN, ColorTheme.GRAY, ColorTheme.RED, ColorTheme.BLUE, ColorTheme.PURPLE, ColorTheme.ORANGE, ColorTheme.HALLOWEEN};
+        return new CharSequence[]{com.xenoamess.partaker.data.ColorTheme.GITHUB, com.xenoamess.partaker.data.ColorTheme.MODERN, com.xenoamess.partaker.data.ColorTheme.GRAY, com.xenoamess.partaker.data.ColorTheme.RED, com.xenoamess.partaker.data.ColorTheme.BLUE, com.xenoamess.partaker.data.ColorTheme.PURPLE, com.xenoamess.partaker.data.ColorTheme.YELLOW, com.xenoamess.partaker.data.ColorTheme.ORANGE, com.xenoamess.partaker.data.ColorTheme.HALLOWEEN, com.xenoamess.partaker.data.ColorTheme.PSYCHEDELIC, com.xenoamess.partaker.data.ColorTheme.MOON};
     }
 
 }
